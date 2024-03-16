5077d8abd9d3a55f3ea371020a15abb5bb1c1852
cadentj <caden.juang@gmail.com>
2024-03-16 09:19:01 -0400

    simulator progress

--- simulator.py
+++ simulator.py
@@ -2,23 +2,32 @@ from typing import List
 from torch import Tensor
 from dataclasses import dataclass
 
+SYSTEM_PROMPT = ""
+ACTION_PROMPT = ""
 
 @dataclass
-
 class Feature:
     tokens: List[str]
     acts: Tensor
     n_acts: Tensor = None
 
+def gen(model, messages, remote=False):
+    with model.generate(messages, max_new_tokens=100, remote=remote):
+        tokens = model.generator.output.save()
+
+    return model.tokenizer.decode(tokens)
+
 class Agent:
 
-    def __init__(self, model):
+    def __init__(self, model, reward_engine):
         self.model = model
+        self.tokenizer = model.tokenizer
 
-        self.prompt_builder = PromptBuilder(model.tokenizer)
+        self.reward_engine = reward_engine
 
-        self.memory = []
         self.scores = []
+        self.memory = None
+        self.trials = {}
 
     def explain(self, feature: Feature):
         """Generate a detailed explaination of feature activations
@@ -30,24 +39,45 @@ class Agent:
             explaination (str): A detailed explaination of the feature activations
         """
         
-        prompt = ""
+        if not self.memory:
+            
+            system = {
+                "role" : "system",
+                "content" : SYSTEM_PROMPT
+            }
 
-        with self.model.generate(prompt, max_new_tokens=100):
-            tokens = self.model.generator.output.save() 
+            environment = {
+                "role" : "user",
+                "content" : self.generate_environment(feature)
+            }
 
-        decoded_tokens = self.model.tokenizer.decode(tokens)
+            messages = [system, environment]
+        else: 
+            messages = self.memory
+            # TODO: no idea
 
-    def action(self):
+        return gen(self.model, messages)
+
+    def generate_environment(self, feature: Feature):
+        pass
+
+
+    def action(self, messages: str):
         """Given some explaination, generate phrases which maximize activations
 
         Args: 
-            explaination (str): A detailed explaination of the feature activations
+            messages (str): Message history with the most recent being an explaination
 
         Returns:
             phrases (List[str]): Phrases which maximize activations
         """
-        pass
+        
+        messages.append({
+            "role" : "user",
+            "content" : ACTION_PROMPT
+        })
 
+        return gen(self.model, messages)
 
     def parse_action(self):
         """Given some phrases, parse them into scorable tokens for the RewardEngine
@@ -82,19 +112,19 @@ class Agent:
         """
         pass
 
-class PromptBuilder:
+    def __call__(self, feature: Feature):
 
-    def __init__(self, tokenizer):
-        self.tokenizer = tokenizer
+        explaination = self.explain(feature)
 
-    def build_explaination(self):
-        pass
+        action = self.action(explaination)
+        parsed_action = self.parse_action(action)
 
-    def build_action(self):
-        pass
+        reward = self.reward_engine.score(parsed_action)
+        self.scores.append(reward)
 
-    def build_reflection(self):
-        pass
+        reflection = self.reflect(reward)
+
+        self.trial[0] = reflection
 
 class RewardEngine:
 
