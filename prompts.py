e092dedfa0f8fc84ad8201250d4875820d0d715b
cadentj <caden.juang@gmail.com>
2024-03-18 18:22:19 -0400

    restructuring

--- prompts.py
+++ prompts.py
@@ -1,51 +1,141 @@
-SYSTEM_PROMPT =  """We're studying neurons in a neural network. Each neuron looks for some particular thing in a short document. Look at the parts of the document the neuron activates for and think step by step about what the neuron is doing. 
+from langchain.prompts import PromptTemplate
 
-The activation format is token<tab>activation. Activation values range from 0 to 10. A neuron finding what it's looking for is represented by a non-zero activation value. The higher the activation value, the stronger the match.
+COT_INSTRUCTION = """Solve a question answering task by having a Thought, then Finish with your answer. Thought can reason about the current situation. Finish[answer] returns the answer and finishes the task. You will be given context that you should use to help you answer the question.
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
+{reflections}
+Relevant Context: {context} 
+Question: {question}{scratchpad}"""
 
-(EXAMPLE USER)
-Sentence: abcdef
+COT_AGENT_REFLECT_INSTRUCTION = """Solve a question answering task by having a Thought, then Finish with your answer. Thought can reason about the current situation. Finish[answer] returns the answer and finishes the task. You will be given context that you should use to help you answer the question.
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
 
-Activations:
-<start>
-a	10
-b	0
-c	0
-d	0
-e	10
-f	0
-<end>
+{reflections}
 
-(EXAMPLE MODEL)
-- Activations are high on letters a and e which are vowels
-- Letters for which activations are low are consonants
-- This neuron activates on vowels
+Relevant Context: {context}
+Question: {question}{scratchpad}"""
 
-(USER)
-Sentence: {sentence}
+COT_REFLECT_INSTRUCTION = """You are an advanced reasoning agent that can improve based on self refection. You will be given a previous reasoning trial in which you were given access to relevant context and a question to answer. You were unsuccessful in answering the question either because you guessed the wrong answer with Finish[<answer>] or there is a phrasing discrepancy with your provided answer and the answer key. In a few sentences, Diagnose a possible reason for failure or phrasing discrepancy and devise a new, concise, high level plan that aims to mitigate the same failure. Use complete sentences.  
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
 
-Activations:
-<start>
-{activations}
-<end>
+Previous trial:
+Relevant Context: {context}
+Question: {question}{scratchpad}
 
-(MODEL)
-"""
+Reflection:"""
 
-ACTION_PROMPT = """Now, write three sentences that would maximize the activations of the neuron. Return your sentences in brackets.
+cot_agent_prompt = PromptTemplate(
+                        input_variables=["examples", "reflections", "context", "question", "scratchpad"],
+                        template = COT_INSTRUCTION,
+                        )
 
-(EXAMPLE MODEL)
-{sentence1}
-{sentence2}
-{sentence3}
+cot_reflect_agent_prompt = PromptTemplate(
+                        input_variables=["examples", "reflections", "context", "question", "scratchpad"],
+                        template = COT_AGENT_REFLECT_INSTRUCTION,
+                        )
 
-(MODEL)
-"""
+cot_reflect_prompt = PromptTemplate(
+                        input_variables=["examples", "context", "question", "scratchpad"],
+                        template = COT_REFLECT_INSTRUCTION,
+                        )
 
-RE_EXPLAIN_PROMPT = "Given the self reflections..."
+COT_SIMPLE_INSTRUCTION = """Solve a question answering task by having a Thought, then Finish with your answer. Thought can reason about the current situation. Finish[answer] returns the answer and finishes the task.
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
+{reflections}
+{context}
+Question: {question}{scratchpad}"""
 
-REFLECTION_PROMPT = """You will be given the history of a past experience in which you were placed in an environment and given a task to complete. You were unsuccessful in completing the task. Do not summarize your environment, but rather think about the strategy and path you took to attempt to complete the task. Devise a concise, new plan of action that accounts for your mistake with reference to specific actions that you should have taken. For example, if you tried A and B but forgot C, then devise a plan to achieve C with environment-specific actions. You will need this later when you are solving the same task. Give your plan after "Plan". Here are two examples:
+COT_SIMPLE_AGENT_REFLECT_INSTRUCTION = """Solve a question answering task by having a Thought, then Finish with your answer. Thought can reason about the current situation. Finish[answer] returns the answer and finishes the task.
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
+{context}
+{reflections}
 
-{trial}
+Question: {question}{scratchpad}"""
 
-Given your score, """
+COT_SIMPLE_REFLECT_INSTRUCTION = """You are an advanced reasoning agent that can improve based on self refection. You will be given a previous reasoning trial in which you were given a question to answer. You were unsuccessful in answering the question either because you guessed the wrong answer with Finish[<answer>] or there is a phrasing discrepancy with your provided answer and the answer key. In a few sentences, Diagnose a possible reason for failure or phrasing discrepancy and devise a new, concise, high level plan that aims to mitigate the same failure. Use complete sentences.
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
+{context}
+Previous trial:
+Question: {question}{scratchpad}
+
+Reflection:"""
+
+cot_simple_agent_prompt = PromptTemplate(
+                        input_variables=["examples", "question", "reflections", "context", "scratchpad"],
+                        template = COT_SIMPLE_INSTRUCTION,
+                        )
+
+cot_simple_reflect_agent_prompt = PromptTemplate(
+                        input_variables=["examples", "context", "reflections", "question", "scratchpad"],
+                        template = COT_SIMPLE_AGENT_REFLECT_INSTRUCTION,
+                        )
+
+cot_simple_reflect_prompt = PromptTemplate(
+                        input_variables=["examples", "question", "context", "scratchpad"],
+                        template = COT_SIMPLE_REFLECT_INSTRUCTION,
+                        )
+
+
+REACT_INSTRUCTION = """Solve a question answering task with interleaving Thought, Action, Observation steps. Thought can reason about the current situation, and Action can be three types: 
+(1) Search[entity], which searches the exact entity on Wikipedia and returns the first paragraph if it exists. If not, it will return some similar entities to search.
+(2) Lookup[keyword], which returns the next sentence containing keyword in the last passage successfully found by Search.
+(3) Finish[answer], which returns the answer and finishes the task.
+You may take as many steps as necessary.
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
+Question: {question}{scratchpad}"""
+
+REACT_REFLECT_INSTRUCTION = """Solve a question answering task with interleaving Thought, Action, Observation steps. Thought can reason about the current situation, and Action can be three types: 
+(1) Search[entity], which searches the exact entity on Wikipedia and returns the first paragraph if it exists. If not, it will return some similar entities to search.
+(2) Lookup[keyword], which returns the next sentence containing keyword in the last passage successfully found by Search.
+(3) Finish[answer], which returns the answer and finishes the task.
+You may take as many steps as necessary.
+Here are some examples:
+{examples}
+(END OF EXAMPLES)
+
+{reflections}
+
+Question: {question}{scratchpad}"""
+
+REFLECTION_HEADER = 'You have attempted to answer following question before and failed. The following reflection(s) give a plan to avoid failing to answer the question in the same way you did previously. Use them to improve your strategy of correctly answering the given question.\n'
+REFLECTION_AFTER_LAST_TRIAL_HEADER = 'The following reflection(s) give a plan to avoid failing to answer the question in the same way you did previously. Use them to improve your strategy of correctly answering the given question.\n'
+LAST_TRIAL_HEADER = 'You have attempted to answer the following question before and failed. Below is the last trial you attempted to answer the question.\n'
+
+REFLECT_INSTRUCTION = """You are an advanced reasoning agent that can improve based on self refection. You will be given a previous reasoning trial in which you were given access to an Docstore API environment and a question to answer. You were unsuccessful in answering the question either because you guessed the wrong answer with Finish[<answer>], or you used up your set number of reasoning steps. In a few sentences, Diagnose a possible reason for failure and devise a new, concise, high level plan that aims to mitigate the same failure. Use complete sentences.  
+Here are some examples:
+{examples}
+
+Previous trial:
+Question: {question}{scratchpad}
+
+Reflection:"""
+
+react_agent_prompt = PromptTemplate(
+                        input_variables=["examples", "question", "scratchpad"],
+                        template = REACT_INSTRUCTION,
+                        )
+
+react_reflect_agent_prompt = PromptTemplate(
+                        input_variables=["examples", "reflections", "question", "scratchpad"],
+                        template = REACT_REFLECT_INSTRUCTION,
+                        )
+
+reflect_prompt = PromptTemplate(
+                        input_variables=["examples", "question", "scratchpad"],
+                        template = REFLECT_INSTRUCTION,
+                        )
+
 
--- simulator.py
+++ simulator.py
@@ -3,7 +3,39 @@ from torch import Tensor
 import torch
 from dataclasses import dataclass
 import re
-from prompts import SYSTEM_PROMPT, ACTION_PROMPT, RE_EXPLAIN_PROMPT, REFLECTION_PROMPT
+
+SYSTEM_PROMPT =  """We're studying neurons in a neural network. Each neuron looks for some particular thing in a short document. Look at the parts of the document the neuron activates for and give a detailed explaination about what the neuron is doing. 
+
+The activation format is token<tab>activation. Activation values range from 0 to 10. A neuron finding what it's looking for is represented by a non-zero activation value. The higher the activation value, the stronger the match.
+
+Sentence: 
+{sentence}
+
+Activations:
+<start>
+{activations}
+<end>
+
+Here are three concise sentences that best describe the neurons behavior.
+"""
+
+ACTION_PROMPT = """Given your observations, write three sentences that would maximize the activations of the neuron. Return your sentences in brackets.
+
+Example format:
+{sentence1}
+{sentence2}
+{sentence3}
+
+Your sentences:
+"""
+
+RE_EXPLAIN_PROMPT = "Given the self reflections..."
+
+REFLECTION_PROMPT = """You will be given the history of a past experience in which you were placed in an environment and given a task to complete. You were unsuccessful in completing the task. Do not summarize your environment, but rather think about the strategy and path you took to attempt to complete the task. Devise a concise, new plan of action that accounts for your mistake with reference to specific actions that you should have taken. For example, if you tried A and B but forgot C, then devise a plan to achieve C with environment-specific actions. You will need this later when you are solving the same task. Give your plan after "Plan". Here are two examples:
+
+{trial}
+
+Given your score, """
 
 @dataclass 
 class Location:
@@ -13,6 +45,7 @@ class Location:
 
 @dataclass
 class Feature:
+    prompt: str
     tokens: List[str]
     acts: Tensor
     n_acts: Tensor = None
@@ -25,6 +58,41 @@ def gen(model, messages, remote=False):
         tokens = model.generator.output.save()
 
     return model.tokenizer.decode(tokens[0])
+    
+def flatten_conversation(conversation):
+    flattended = ""
+
+    for message in conversation:
+        if message["role"] == "user":
+            flattended += f"(USER)\n{message['content']}\n\n"
+        else:
+            flattended += f"(MODEL)\n{message['content']}\n\n"
+
+    return flattended[:-2]
+
+class Environment:
+
+    def __init__(self, model, dictionaries):
+        self.model = model
+
+        self.mem = []
+        self.trials = []
+    
+    def load(self):
+
+        self.self_reflector = SelfReflector(self.model)
+        self.reward_engine = Evaluator(self.model, self.dictionaries)
+        self.agent = Agent(self.model, self.reward_engine)
+
+    def explain(self, feature: Feature):
+        
+        success = False
+
+        while not success:
+            action = self.agent(feature)
+
+            
+
 
 class Agent:
 
@@ -38,15 +106,6 @@ class Agent:
         self.trials = []
 
     def explain(self, feature: Feature):
-        """Generate a detailed explaination of feature activations
-
-        Args:
-            feature (Feature): The feature to explain, unnormalized activations
-
-        Returns:
-            explaination (str): A detailed explaination of the feature activations
-        """
-        
         if not self.scores:
             environment = {
                 "role" : "user",
@@ -65,30 +124,13 @@ class Agent:
         return gen(self.model, messages)
 
     def generate_environment(self, feature: Feature):
-        """Generate initial tabulated format for feature activations.
 
-        Args:
-            feature (Feature): The feature to explain, unnormalized activations
-
-        Returns:
-            environment (str): Initial tabulated format for feature activations
-        """
-
-        formatted_tokens = [f"{t}   {a}" for t, a in zip(feature.tokens, feature.acts)]
+        formatted_tokens = [f"{t}   {a}" for t, a in zip(feature.tokens, feature.n_acts)]
         activations = "\n".join(formatted_tokens)
 
-        return SYSTEM_PROMPT.format(sentence="abacdqei", activations=activations)
+        return SYSTEM_PROMPT.format(sentence=feature.prompt, activations=activations)
 
     def action(self, messages: str, regenerate=False):
-        """Given some explaination, generate phrases which maximize activations
-
-        Args: 
-            messages (str): Message history with the most recent being an explaination
-
-        Returns:
-            phrases (List[str]): Phrases which maximize activations
-        """
-        
         if regenerate:
             messages = messages[:-1]
 
@@ -100,86 +142,65 @@ class Agent:
         return gen(self.model, messages)
 
     def parse_action(self, phrase):
-        """Given some phrases, parse them into scorable tokens for the RewardEngine
-
-        Args:
-            phrase (str): Phrase which maximize activations
-
-        Returns:
-            parsed_phrase (str): Parsed phrase
-        """
         pattern = r"\{(.+?)\}"
         parsed_phrase = re.findall(pattern, phrase)
         if not parsed_phrase:
             return False
         return parsed_phrase
-
-    def reflect(self):
-        """Reflect on the score and update long term memory
-
-        Args:
-            score (float): The score of the tokens
-        
-        Returns:
-            None
-        """
-
-        flattened = flatten_conversation(self.trails[-1])
-
-        return REFLECTION_PROMPT.format(trial=flattened)
-        
         
     def __call__(self, feature: Feature):
 
-        # LOOP THIS
         explaination = self.explain(feature)
 
-        return explaination
+        action = self.action(explaination)
+        parsed_phrase = self.parse_action(action)
 
-        # action = self.action(explaination)
-        # parsed_phrase = self.parse_action(action)
+        while (not parsed_phrase):
+            action = self.action(explaination, regenerate=True)
+            parsed_phrase = self.parse_action(action)
 
-        # while (not parsed_phrase):
-        #     action = self.action(explaination, regenerate=True)
-        #     parsed_phrase = self.parse_action(action)
+        return parsed_phrase
 
-        # reward = self.reward_engine.score(feature, parsed_phrase)
-        # self.scores.append(reward)
+class SelfReflector:
 
-        # reflection = self.reflect(reward)
+    def __init__(self, model):
+        self.model = model
 
-        # self.trials.append(reflection)
+    def reflect(self, messages):
+        return gen(self.model, messages)
 
-def flatten_conversation(conversation):
-    flattended = ""
 
-    for message in conversation:
-        if message["role"] == "user":
-            flattended += f"(USER)\n{message['content']}\n\n"
-        else:
-            flattended += f"(MODEL)\n{message['content']}\n\n"
-
-    return flattended[:-2]
-
-class RewardEngine:
+class Evaluator:
 
     def __init__(self, model, dictionaries, threshold=8):
         self.model = model
         self.dictionaries = dictionaries
 
+    def evaluate(self, trials):
+
+        flattened = flatten_conversation(trials[-1])
+
+        prompt = REFLECTION_PROMPT.format(trial=flattened)
+
+        return gen(self.model, prompt)
+
     def score(self, feature, parsed_phrase):
-        activation = self.get_activation(feature.location)
+        acts = self.get_activation(feature.location)
 
         # TODO: SOME SCORING
         return 0
 
-    def get_activation(self, location, phra): 
+    def get_activation(self, location, phrase): 
 
-        # with torch.no_grad():
+        layer = location.layer
+        index = location.index
 
-        #     with self.model.invoke()
+        with self.model.trace(phrase):
+            activations = self.model.transformer.h[layer].output[0]
+
+            _, feature_acts, _, _, _, _ = self.dictionaries[layer](activations)
+            
+            acts = feature_acts[:,:,index].save()
+
+        return acts
         
-        # return acts[location.index]
-
-        pass
-
