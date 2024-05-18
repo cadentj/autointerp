97ab1226372fe958b434be10cb4555084398ec1b
cadentj <caden.juang@gmail.com>
2024-05-18 16:11:28 -0400

    remove yaml

--- working/__init__.py
+++ working/__init__.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import openai
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -7,3 +8,6 @@ from .pydantics import ConfigModel
 
 with open(os.path.join(PATH, "config.yaml"), "r") as file:
     CONFIG = ConfigModel(**yaml.safe_load(file))
+
+if CONFIG is openai:
+    CLIENT = openai.Client(CONFIG.API.APIKEY)
--- working/prompt.py
+++ working/prompt.py
@@ -1,2 +1,30 @@
+from abc import ABC, abstractmethod
+from . import CONFIG
 import openai
 
+class Client(ABC):
+    def __init__(self, model: str):
+        self.model = model
+
+    @abstractmethod
+    def generate(self, prompt: str) -> str:
+        pass
+
+class OpenAI(Client):
+    def __init__(self, model: str, api_key: str):
+        super().__init__(model)
+        self.client = openai.OpenAI(api_key=api_key)
+    
+    def generate(self, prompt: str) -> str:
+        return self.client.chat.completions.create(
+            model=self.model,
+            messages=[{"role": "system", "content": prompt}]
+        ).choices[0].message.content
+
+def get_client(provider: str, api_key: str):
+    if provider == "openai":
+        return OpenAI(provider, api_key)
+    
+def create(prompt: str):
+    if CONFIG.API.PROVIDER == "openai":
+        return OpenAI(CONFIG.API.PROVIDER, CONFIG.API.APIKEY).generate(prompt)
