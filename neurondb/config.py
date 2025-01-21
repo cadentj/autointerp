878fff841176d0ee46aab3ec49b1597d5cdcc90f
cadentj <caden.juang@gmail.com>
2025-01-21 04:48:42 +0000

    pyproject

--- neurondb/config.py
+++ neurondb/config.py
@@ -1,6 +1,8 @@
 import yaml
+import os
 
-with open("config.yaml", "r") as f:
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(CURRENT_DIR, "config.yaml"), "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
     config["token"] = "sk-np-gpbPGO04zb8EUBH0XHY3hwoNcds0SjSBx7LwWOYaXXA0"
 
